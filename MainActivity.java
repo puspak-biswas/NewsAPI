e660b5efcc4df5f4fc3fa60d60f98722476f8114
puspak-biswas <38313124+puspak-biswas@users.noreply.github.com>
2018-07-03 09:15:29 +0530

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.puspakbiswas.newsapi;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,14 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Context context = getApplicationContext();
         //ListView listview = (ListView) findViewById(R.id.list_item);
         //ArrayAdapter<String> newAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,R.array.content);
         Button search = (Button) findViewById(R.id.search);
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText editText = (EditText) findViewById(R.id.experiment);
-                editText.setText("done");
+                String[] author = new String[1];
+                author[0] = "author";
+                News news[] = {new News("Title", "Section", "Puspak", author),
+                        new News("Title", "Section", "Puspak", author)};
+                ArrayAdapter<News> theAdapter = new ArrayAdapter<News>(context, android.R.layout.simple_list_item_1, news);
+                ListView listview = (ListView) findViewById(R.id.list_item);
+                listview.setAdapter(theAdapter);
             }
         };
         search.setOnClickListener(listener);
