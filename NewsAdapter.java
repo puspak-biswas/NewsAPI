f10e5bcee6c6c637ff345646ab10bc378eee0c4c
puspak-biswas <38313124+puspak-biswas@users.noreply.github.com>
2018-07-06 09:07:40 +0530

    Update NewsAdapter.java

--- NewsAdapter.java
+++ NewsAdapter.java
@@ -1,20 +1,36 @@
-public class NewsAdapter extends ArrayAdapter<News>{
+package com.example.puspakbiswas.newsapi;
 
-    private ArrayList<news> mNewsList;
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
-    public NewsAdapter(Context context, ArrayList<News> newsList){
-      super(context,0,newsList);
-      mNewsList = newsList;
+import java.util.ArrayList;
+
+/**
+ * Created by Puspak Biswas on 04-07-2018.
+ */
+
+public class NewsAdapter extends ArrayAdapter<News> {
+
+    public NewsAdapter(Activity activity, ArrayList<News> newsList){
+        super(activity,0,newsList);
     }
-    
-    
-    public View getView(Context context,View view, int position{
-      if (view == null){
-        view = inflate(R.id.list_item);
-      }
-      
-      News news = mNewsList.getItem(position);
-      
-      
+
+    public View getView(final int position, View convertView, ViewGroup parent){
+        View listItem = convertView;
+            if (listItem == null){
+                listItem = LayoutInflater.from(getContext()).inflate(
+                        R.layout.list_item,parent,false);
+            }
+
+        News news = getItem(position);
+        TextView title = (TextView) listItem.findViewById(R.id.title);
+        title.setText(news.getTitle());
+        return listItem;
+
     }
 }
